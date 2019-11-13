d367a65294cfa6d9c8e69ed23420785475c2d39a
SevenLightnapper <dairenray@gmail.com>
2019-11-13 20:24:15 +0300

    HW01

---
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -0,0 +1,53 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+public class MealTo {
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+//    private final Supplier<Boolean> excess;
+//    private final AtomicBoolean excess;
+    private final boolean excess;
+
+    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+
+//    public Boolean getExcess() {
+//        return excess.get();
+//    }
+
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "MealTo{" +
+                "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                ", excess=" + excess +
+                '}';
+    }
+}
