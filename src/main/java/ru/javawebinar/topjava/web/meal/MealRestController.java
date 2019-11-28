386168e8342ce91ba6f7d437a1a1667ab634fa6e
SevenLightnapper <dairenray@gmail.com>
2019-11-29 02:20:42 +0300

    HW02_Optional

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -9,6 +9,7 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.time.LocalDate;
@@ -29,13 +30,13 @@ public class MealRestController {
         this.service = service;
     }
 
-    public Meal get(int id) {
+    public Meal get(int id) throws NotFoundException {
         int userId = SecurityUtil.authUserId();
         log.info("get meal {} for user {}", id, userId);
         return service.get(id, userId);
     }
 
-    public void delete(int id) {
+    public void delete(int id) throws NotFoundException {
         int userId = SecurityUtil.authUserId();
         log.info("delete meal {} for user {}", id, userId);
         service.delete(id, userId);
@@ -47,14 +48,14 @@ public class MealRestController {
         return MealsUtil.getTos(service.getAll(userId), SecurityUtil.authUserCaloriesPerDay());
     }
 
-    public Meal create(Meal meal) {
+    public Meal create(Meal meal) throws NotFoundException {
         int userId = SecurityUtil.authUserId();
         checkNew(meal);
         log.info("create {} for user {}", meal, userId);
         return service.create(meal, userId);
     }
 
-    public void update(Meal meal, int id) {
+    public void update(Meal meal, int id) throws NotFoundException {
         int userId = SecurityUtil.authUserId();
         assureIdConsistent(meal, id);
         log.info("update {} for user {}", meal, userId);
